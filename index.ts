abb4bee7c7e6f5be0a153c4904af79f9d27e8892
Zeeshan Bhati <53969232+zeeshanbhati@users.noreply.github.com>
2023-12-26 18:03:41 +0000

    add redis

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import "./tracer";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import mysql from 'mysql2/promise';
+import * as redis from 'redis';
 
 
 
@@ -11,6 +12,10 @@ const app: Express = express();
 const port = process.env.PORT;
 
 const conn = mysql.createPool(process.env.DB_URL || "");
+
+const redisClient  = redis.createClient({
+  url : process.env.REDIS_URL
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server Hello World This is SOmething");
@@ -23,6 +28,11 @@ app.get("/users",   async (req: Request, res: Response) => {
   }
   res.status(400).json({ error: "Notfound" });
 });
+
+app.get("/redis",async(req:Request , res:Response)=>{
+  const response = await redisClient.get('demo');
+  return res.status(200).json(response);
+})
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
