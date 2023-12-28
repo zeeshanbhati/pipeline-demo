fdb5ad79787dc70a8d5baa64021f5ecb57466246
Zeeshan Bhati <53969232+zeeshanbhati@users.noreply.github.com>
2023-12-28 09:47:16 +0000

    Close redis connection

--- index.ts
+++ index.ts
@@ -32,6 +32,7 @@ app.get("/users",   async (req: Request, res: Response) => {
 app.get("/redis",async(req:Request , res:Response)=>{
   const conn = await redisClient.connect();
   const response = await conn.get('demo');
+  conn.disconnect();
   return res.status(200).json(response);
 })
 
