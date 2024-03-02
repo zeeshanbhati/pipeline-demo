0b13a5211d71a6f9c70420c003473a10a1962885
Zeeshan Bhati <zeeshanbhati99@gmail.com>
2024-03-03 01:13:34 +0530

    Remove mysql db

--- index.ts
+++ index.ts
@@ -1,44 +1,42 @@
 import "./tracer";
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-import mysql from 'mysql2/promise';
-import * as redis from 'redis';
-
-
+import mysql from "mysql2/promise";
+import * as redis from "redis";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-const conn = mysql.createPool(process.env.DB_URL || "");
+// const conn = mysql.createPool(process.env.DB_URL || "");
 
-const redisClient  = redis.createClient({
-  url : process.env.REDIS_URL
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
 });
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server Hello World This is SOmething");
 });
 
-app.get("/health", (req:Request , res:Response)=>{
-res.status(200).send('healthy');
-})
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).send("healthy");
+});
 
-app.get("/users",   async (req: Request, res: Response) => {
- const r = await conn.query("SELECT * FROM users");
-  if(r[0]){
+app.get("/users", async (req: Request, res: Response) => {
+  const r = await conn.query("SELECT * FROM users");
+  if (r[0]) {
     return res.status(200).json(r[0]);
   }
   res.status(400).json({ error: "Notfound" });
 });
 
-app.get("/redis",async(req:Request , res:Response)=>{
+app.get("/redis", async (req: Request, res: Response) => {
   const conn = await redisClient.connect();
-  const response = await conn.get('demo');
+  const response = await conn.get("demo");
   conn.disconnect();
   return res.status(200).json(response);
-})
+});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
