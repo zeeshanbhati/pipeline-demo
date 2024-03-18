d59a50b5bc33dda9227ebf6fb375d19ea6a7b728
Zeeshan Bhati <53969232+zeeshanbhati@users.noreply.github.com>
2024-03-18 21:05:52 +0000

    Run in detach mode

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 849857290067.dkr.ecr.ap-south-1.amazonaws.com
 
-docker run -p 3000:3000 --name express-pipeline 849857290067.dkr.ecr.ap-south-1.amazonaws.com/express-pipeline:53
+docker run -d -p 3000:3000 --name express-pipeline 849857290067.dkr.ecr.ap-south-1.amazonaws.com/express-pipeline:53
